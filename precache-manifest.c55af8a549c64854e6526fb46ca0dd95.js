69b794b1c9ab14d23f44d0798a02694a9eabe889
raman pratap <ramanprtap>
2020-06-01 19:21:41 +0530

    Updates

---
+++ precache-manifest.c55af8a549c64854e6526fb46ca0dd95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea303dff71e49cdf14529b26628bd898",
+    "url": "/ramanprtap/weather-demo/index.html"
+  },
+  {
+    "revision": "e23de20a89f01627c367",
+    "url": "/ramanprtap/weather-demo/static/css/main.15cfaa6d.chunk.css"
+  },
+  {
+    "revision": "1faad7f255ee893c55bc",
+    "url": "/ramanprtap/weather-demo/static/js/2.a4fb13ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ramanprtap/weather-demo/static/js/2.a4fb13ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e23de20a89f01627c367",
+    "url": "/ramanprtap/weather-demo/static/js/main.7ba1014c.chunk.js"
+  },
+  {
+    "revision": "96e268ad2ea13a13c2c7",
+    "url": "/ramanprtap/weather-demo/static/js/runtime-main.d343fbf2.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/ramanprtap/weather-demo/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/ramanprtap/weather-demo/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
